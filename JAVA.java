527dbe529ecfe1f739a3d16210da3bf8f9dce038
MateusLima <165662012+GoatMateusLima@users.noreply.github.com>
2025-02-21 12:15:15 -0300

    Create JAVA.java
    aaa

---
+++ JAVA.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+        
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.println("SEJA BEM VINDO");
+        System.out.println("------------------------------------------------------");
+        System.out.println("Estamos felizes em voce fazer parte do nosso banco");
+        System.out.println("------------------------------------------------------");
+        
+        
+        
+        System.out.print("Digite seu nome:");
+        String nome = scanner.next();
+        
+        System.out.print("Digite seu idade:");
+        String idade = scanner.next();
+        
+        System.out.print("Digite seu numero:");
+        String numero = scanner.next();
+        
+        System.out.print("Digite seu email:");
+        String email = scanner.next();
+        
+        System.out.print("Digite seu endereço:");
+        String endereco = scanner.next();
+        
+        
+        System.out.println("------------------------------------------------------");
+        System.out.println("ESTAMOS FELIZES COM SEU CADASTRO EM NOSSO BANCO ;)");
+        System.out.println("------------------------------------------------------");
+        System.out.println("SEJA BEM VINDO AO KINGBANK *****");
+        System.out.println("------------------------------------------------------");
+        System.out.println("SEUS DADOS:");
+        
+        System.out.println("NOME:" + nome);
+        System.out.println("IDADE:" + idade);
+        System.out.println("NUMERO:" + numero);
+        System.out.println("EMAIL:" + email);
+        System.out.println("ENDEREÇO:" + endereco);
+      
